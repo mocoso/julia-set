640f9a293390ccf5648311d2d4939834343517a8
Joel Chippindale <joel@joelchippindale.com>
2018-06-30 08:23:47 +0000

    Calculate plane first and then image

--- juliaset.jl
+++ juliaset.jl
@@ -7,10 +7,10 @@ include("julia.jl")
 # create a 1000x500 Array for our picture
 h = 800
 w = 1600
-img = Array{RGB}(h, w)
+plane = Array{Complex{Float64}}(h, w)
 maxiteration = 255
 
-palette = diverging_palette(200.0, 30.0, maxiteration + 1, mid=0.3)[1:maxiteration]
+palette = diverging_palette(200.0, 30.0, maxiteration + 1, mid = 0.3)[1:maxiteration]
 
 function colour_from_palette(palette, n)
     if n == maxiteration
@@ -24,18 +24,17 @@ end
 print("starting...\n")
 tStart=time()
 
-# for every pixel
-for y=1:h, x=1:w
-    # translate numbers [1:w, 1:h] -> -2:2 + -1:1 im
-    c = complex((x-w/2)/(h/2), (y-h/2)/(h/2))
-    # call our julia function
-    iterations = UInt8(julia(c, maxiteration))
-    # translate to a colour
-    img[y,x] = colour_from_palette(palette, iterations)
+# for every pixel calcuate the complex number it represents
+for y = 1:h
+    # translate numbers indexes -> -2:2 + -1:1 im
+    plane[y, :] = linspace(complex(-w/h, (y-h/2)/(h/2)), complex(w/h, (y-h/2)/(h/2)), w)
 end
+
+# create an image from the plane
+img = map(c -> colour_from_palette(palette, julia(c, maxiteration)), plane)
 
 tStop = time()
 
 save("image.png", img)
 
-print("done. took ", tStop-tStart, " seconds\n");
+print("done. took ", tStop - tStart, " seconds\n");

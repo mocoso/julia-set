b4217b44925c60cba9ec9ac00e4a478ce4867005
Joel Chippindale <joel@joelchippindale.com>
2018-06-30 07:30:57 +0000

    Move from grey scale to color
    With some code modified from http://distrustsimplicity.net/articles/julia-in-julia/

--- juliaset.jl
+++ juliaset.jl
@@ -7,8 +7,18 @@ include("julia.jl")
 # create a 1000x500 Array for our picture
 h = 800
 w = 1600
-m = Array{Float64,2}(h, w)
-maxiteration = 256
+img = Array{RGB}(h, w)
+maxiteration = 255
+
+palette = diverging_palette(200.0, 30.0, maxiteration + 1, mid=0.3)[1:maxiteration]
+
+function colour_from_palette(palette, n)
+    if n == maxiteration
+        RGB(0,0,0)
+    else
+        palette[n + 1]
+    end
+end
 
 # time measurements
 print("starting...\n")
@@ -19,11 +29,13 @@ for y=1:h, x=1:w
     # translate numbers [1:w, 1:h] -> -2:2 + -1:1 im
     c = complex((x-w/2)/(h/2), (y-h/2)/(h/2))
     # call our julia function
-    m[y,x] = julia(c, maxiteration) / maxiteration
+    iterations = UInt8(julia(c, maxiteration))
+    # translate to a colour
+    img[y,x] = colour_from_palette(palette, iterations)
 end
 
 tStop = time()
 
-save("image.png", m)
+save("image.png", img)
 
 print("done. took ", tStop-tStart, " seconds\n");
